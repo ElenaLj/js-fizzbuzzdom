d1562c2acf5398d49a9c12cdfc33332fc802eb64
ElenaLj <elenaljubijankic@gmail.com>
2021-11-09 17:37:20 +0100

    insert milestone 3

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 //Milestone 1: Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 //Milestone 2: Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+//Milestone 3: Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
 //Dichiaro e assegno variabile
 const ul = document.querySelector("ul.number")
