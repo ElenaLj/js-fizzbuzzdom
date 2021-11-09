c6c6c8146dc18aa34522c70673afe334bdca2c5d
ElenaLj <elenaljubijankic@gmail.com>
2021-11-09 17:57:08 +0100

    insert classi <li> in js

--- js/script.js
+++ js/script.js
@@ -9,19 +9,25 @@ const ul = document.querySelector("ul.number")
 
 //iterazione for
 for (let i = 1; i <= 100; i++) {
-    const elemento = `<li class="box box--${i}">${i}</li>`;
-    ul.innerHTML += elemento;
-
+    
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("FizzBuzz");
+        const multiplo15 = `<li class="box box--${i}">FizzBuzz</li>`;
+        ul.innerHTML += multiplo15;
     }
     else if (i % 3 == 0) {
         console.log("Fizz");
+        const multiplo3 = `<li class="box box--${i}">Fizz</li>`;
+        ul.innerHTML += multiplo3;
     }
     else if (i % 5 == 0) {
         console.log("Buzz");
+        const multiplo5 = `<li class="box box--${i}">Buzz</li>`;
+        ul.innerHTML += multiplo5;
     }
     else {
         console.log(i);
+        const numero = `<li class="box box--${i}">${i}</li>`;
+        ul.innerHTML += numero;
     }
 };
