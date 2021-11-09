658f9c2f19e5ed15d08d3737c72fd17a743b6381
ElenaLj <elenaljubijankic@gmail.com>
2021-11-09 16:30:56 +0100

    DOM

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 // Compito: stampa i numeri da 1 a 100
 
 //Milestone 1: Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+//Milestone 2: Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 
+//Dichiaro e assegno variabile
+const ul = document.querySelector("ul.number")
+
+//iterazione for
 for (let i = 1; i <= 100; i++) {
+    const elemento = `<li class="box box--${i}">${i}</li>`;
+    ul.innerHTML += elemento;
+
     if (i % 3 == 0 && i % 5 == 0) {
-        console.log(i + " FizzBuzz");
+        console.log("FizzBuzz");
     }
     else if (i % 3 == 0) {
-        console.log(i + " Fizz");
+        console.log("Fizz");
     }
     else if (i % 5 == 0) {
-        console.log(i + " Buzz");
+        console.log("Buzz");
     }
     else {
         console.log(i);
